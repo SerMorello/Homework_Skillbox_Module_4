8c439a367991454cd58d87de7e3aeb34d336563b
Сережка Стр <morellosharp@gmail.com>
2023-03-19 21:48:19 +0300

    5_1

---
+++ task_5_1/task_5_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics.Tracing;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        string userText = Console.ReadLine();
+        userSplitText(userText);
+    }
+
+    //static void splitText(string text)
+    //{
+    //    string[] arr = text.Split();
+    //    printArr(arr);
+    //}
+
+    private static void printArr(string[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+
+    static void userSplitText(string text)
+    {
+        int count = 0;
+        text = text + ' ';
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == ' ')
+            {
+                count++;
+            }
+        }
+
+        string[] textArr = new string[count];
+        string word = "";
+        int countArr = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] != ' ')
+            {
+                word = word + text[i];
+            }
+            else
+            {
+                textArr[countArr] = word;
+                word = "";
+                countArr++;
+            }
+
+        }
+        printArr(textArr);
+    }
+}
