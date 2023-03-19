1c9510134396a2546f04a7e012d578150867043d
Сережка Стр <morellosharp@gmail.com>
2023-03-20 00:57:33 +0300

    gitignore

---
+++ task_5_2/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string userText = Console.ReadLine();
+        reverseString(userText);
+    }
+
+    private static string[] splitText(string? userText)
+    {
+        string[] text = userText.Split();
+        return text;
+    }
+
+    private static void reverseString(string text)
+    {
+        string[] textArr = splitText(text);
+
+        string resultString = "";
+
+        for (int i = textArr.Length-1; i >= 0; i--)
+        {
+            resultString += textArr[i] + " ";
+        }
+        Console.WriteLine(resultString);
+       
+    }
+}
